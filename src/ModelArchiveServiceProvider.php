abe2d0f615da03d746dbb55a9a42c3fac592a5fc
Tcharod <tcharod21@gmail.com>
2024-08-19 19:12:34 +0100

    init package structure

---
+++ src/ModelArchiveServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lab2view\ModelArchive;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModelArchiveServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
