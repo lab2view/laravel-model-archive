7d03e70ede3d5990a9a1134239f334245c996d68
JOSIAS MEKONTSO <m.js@tutanota.com>
2024-11-18 11:32:42 +0100

    Documentation of the polymorphic relationship allowing to record references to the data that has been archived

--- src/Models/Archive.php
+++ src/Models/Archive.php
@@ -2,12 +2,20 @@
 
 namespace Lab2view\ModelArchive\Models;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Config;
+use Lab2view\ModelArchive\Traits\Archivable;
 
+/**
+ * Class Archive
+ *
+ * @property int $archivable_id
+ * @property class-string<Archivable> $archivable_type
+ * @property array<string> $archive_with
+ */
 class Archive extends Model
 {
     /**
@@ -21,17 +29,18 @@ class Archive extends Model
         'archive_with',
     ];
 
-    public function getConnectionName(){
+    public function getConnectionName()
+    {
         return Config::get('model-archive.archive_db_connection');
     }
-    
+
     /**
      * Interact with the attribute archive_with.
      */
     protected function archiveWith(): Attribute
     {
         return Attribute::make(
-            get: fn (string $value) => json_decode($value),
+            get: fn (string $value) => $value ? json_decode($value) : [] ,
             set: fn (array $value) => json_encode($value),
         );
     }
@@ -44,6 +53,11 @@ class Archive extends Model
         return $this->morphTo();
     }
 
+    /**
+     * Scope request to retrieve archives that have not yet been validated
+     * @param Builder<Archivable> $query
+     * @return Builder<Archivable>
+     */
     public function scopeUnvalidated(Builder $query): Builder
     {
         return $query->whereNull('validated_at');
